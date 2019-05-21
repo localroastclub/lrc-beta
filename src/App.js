a4d11b138cd2cb7fe4d70c8c7d327219cea84c3d
Laura <laura.robertson64@gmail.com>
2019-05-21 16:50:04 -0500

    mapping out the sub header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import Headline from './components/Headline';
+import SubHeader from './components/SubHeader';
+import Grid from '@material-ui/core/Grid';
 
 
 function App() {
   return (
-    <>
-      <NavBar/>
-      <Headline/>
-    </>
+    <Grid
+      container={true}
+      alignItems="center"
+      alignContent="space-between"
+      spacing={24}
+    >
+      <Grid item={true} xs={12}>
+        <NavBar />
+      </Grid>
+      <Grid item={true} xs={12}>
+        <Headline />
+      </Grid>
+      <Grid item={true} xs={12}>  
+        <SubHeader />
+      </Grid>
+    </Grid>
   );
 }
 
--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import HeaderLeft from './HeaderLeft';
-import HeaderRight from './HeaderRight';
 import { withStyles } from '@material-ui/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
 
-const HeaderBox = withStyles({
+const StyledCard = withStyles({
+  root: {
+    background: '#FFF',
+    opacity: 0.5,
+    borderRadius: 3,
+    border: 0,
+    color: 'white',
+    height: '40vh',
+    maxWidth: '55vh',
+    minWidth: '30vh',
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  },
+  label: {
+    textTransform: 'capitalize',
+  },
+})(Card);
+
+const PaperGrid = withStyles({
   root: {
     backgroundColor: '#FFF',
     borderRadius: 3,
@@ -14,21 +30,21 @@ const HeaderBox = withStyles({
     color: 'white',
     minHeight: '40vh',
     padding: '0 30px',
-    boxShadow: '0 3px 5px 2px #686868',
+    boxShadow: 0,
   },
   label: {
     textTransform: 'capitalize',
   },
 })(Paper);
 
-const Headline = () => {
+const SubHeader = () => {
   return(
     <Grid
       container={true}
-      alignItems="stretch"
+      alignItems="center"
     >
       <Grid item xs={12}>
-        <HeaderBox>
+        <PaperGrid>
           <Grid 
             container={true}
             justify="center"
@@ -36,16 +52,19 @@ const Headline = () => {
             spacing={24}
           >
             <Grid item={true}>
-              <HeaderLeft/>
+              <StyledCard/>
             </Grid>
             <Grid item={true}>
-              <HeaderRight/>
+              <StyledCard/>
+            </Grid>
+            <Grid item={true}>
+              <StyledCard/>
             </Grid>
           </Grid>
-        </HeaderBox>
+        </PaperGrid>
       </Grid>
     </Grid>
   )
 }
 
-export default Headline;
+export default SubHeader;
