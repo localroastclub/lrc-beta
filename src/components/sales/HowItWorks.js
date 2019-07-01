acb0ad048a16a629a0c9d3c166dc6ad60401810e
Laura <laura.robertson64@gmail.com>
2019-07-01 11:24:50 -0500

    set up icons for how-it-works cards

--- src/components/sales/HowItWorks.js
+++ src/components/sales/HowItWorks.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { withStyles } from '@material-ui/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import LocalCafe from '@material-ui/icons/LocalCafe';
+import RoomService from '@material-ui/icons/RoomService';
+import Edit from '@material-ui/icons/Edit';
+
+
 
 const StyledCard = withStyles({
   root: {
@@ -9,7 +14,7 @@ const StyledCard = withStyles({
     borderRadius: 3,
     border: 0,
     color: 'black',
-    height: '25vh',
+    height: '40vh',
     width: '30vh',
     padding: '0 30px',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
@@ -23,6 +28,32 @@ const CardText = withStyles({
   }
 })(CardContent)
 
+const OrderIcon = withStyles({
+  root: {
+    color: '#e6b5da',
+    width: '4rem',
+    height: '4rem',
+  }
+})(Edit);
+
+const BellIcon = withStyles({
+  root: {
+    color: '#e6b5da',
+    width: '4rem',
+    height: '4rem',
+  }
+})(RoomService);
+
+const MugIcon = withStyles({
+  root: {
+    color: '#e6b5da',
+    width: '4rem',
+    height: '4rem',
+  }
+})(LocalCafe);
+
+
+
 
 
 const HowItWorks = () => {
@@ -35,24 +66,30 @@ const HowItWorks = () => {
         <StyledCard className="card-copy">
           <CardText>
             <h3>
-              Expand your palette and discover new roasts each month
+            Step 1
             </h3>
+            <OrderIcon />
+            <p>Sign up and select your preference of Roast, Bean, and Roaster</p>
           </CardText>
         </StyledCard>
 
         <StyledCard className="card-copy">
           <CardText>
             <h3>
-              Free up time from traffic and crowded grocery stores
+              Step 2
             </h3>
+            <BellIcon />
+            <p>Orders are roasted to order on the 1st or 3rd Monday each month</p>
           </CardText>
         </StyledCard>
 
         <StyledCard className="card-copy">
           <CardText>
             <h3>
-              Enjoy premium flavors while supporting your local businesses
+              Step 3
             </h3>
+            <MugIcon />
+            <p>Fresh premium coffee is delivered to your door once every month</p>
           </CardText>
         </StyledCard>
       </div>
