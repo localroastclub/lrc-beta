2683e016cf00b52972c9842a38430c8286d19b9d
Laura <laura.robertson64@gmail.com>
2019-07-02 10:14:36 -0500

    adapted carousel to components, styling funky

--- src/components/sales/Reviews.js
+++ src/components/sales/Reviews.js
@@ -1,5 +1,28 @@
 import React from 'react';
-import ReviewCards from './ReviewCards';
+import ReviewCarousel from './ReviewCarousel';
+import './carousel.css'
+
+const carouselSlidesData = [
+  {
+    content:
+      "Tomorrow, you will be released. If you are bored of brawling with thieves and want to achieve something there is a rare blue flower that grows on the eastern slopes. Pick one of these flowers. If you can carry it to the top of the mountain, you may find what you were looking for in the first place.",
+    author: "Bane",
+    avatar: "facebook",
+    location: "Austin, Tx"
+  }, {
+    content:
+      "You have learn to bury your guilt with anger. I will teach you to confront it and to face the truth.",
+    author: "Ra's Al Ghul",
+    avatar: "Snapchat",
+    location: "Austin, Tx"
+  }, {
+    content:
+      "Introduce a little anarchy, upset the established order and everything becomes chaos. I'm an agent of chaos. Oh, and you know the thing about chaos? It's fair.",
+    author: "Joker",
+    avatar: "facebook",
+    location: "Austin, Tx"
+  }
+];
 
 const Reviews = () => {
   return (
@@ -13,7 +36,9 @@ const Reviews = () => {
       ei impetus scaevola contentiones eos, eos ea ridens nominati. Mucius nostrud invenire 
       ex pro, numquam reprimique eum no, quo solum errem sententiae id.
     </p>
-    <ReviewCards />
+    <div className="carousel-container">
+      <ReviewCarousel slides={carouselSlidesData} />
+    </div>
   </div>
   );
 };
