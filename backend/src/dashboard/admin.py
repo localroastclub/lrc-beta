9a21b5b94355870db2a3a9cda38f3175fbac6508
Laura <laura.robertson64@gmail.com>
2019-07-04 13:37:40 -0500

    registered models with django admin

--- backend/src/dashboard/admin.py
+++ backend/src/dashboard/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Users_Login
+from .models import Users_Data
+from .models import Roasters
+from .models import Coffee
+from .models import Orders
+
+
+admin.site.register(Users_Login)
+admin.site.register(Users_Data)
+admin.site.register(Roasters)
+admin.site.register(Coffee)
+admin.site.register(Orders)
--- backend/src/dashboard/models.py
+++ backend/src/dashboard/models.py
@@ -1,31 +1,34 @@
 from django.db import models
 
+
 class Users_Login(models.Model):
-  email = models.CharField(max_length=40)
-  hashword = models.TextField
+    email = models.CharField(max_length=40)
+    hashword = models.TextField
+
 
 class Users_Data(models.Model):
-  firstname = models.CharField(max_length=50)
-  lastname = models.CharField(max_length=50)
-  phone = models.CharField(max_length=10)
-  address = models.CharField(max_length=255)
-  cchash = models.TextField
-  subscribed = models.BooleanField
-  order_rotation_id = models.CharField(max_length=20)
-  user_login = models.ForeignKey(Users_Login, on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    address = models.CharField(max_length=255)
+    cchash = models.TextField
+    subscribed = models.BooleanField
+    order_rotation_id = models.CharField(max_length=20)
+    user_login = models.ForeignKey(Users_Login, on_delete=models.CASCADE)
+
 
 class Roasters(models.Model):
-  name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
 
 class Coffee(models.Model):
-  roast_name = models.CharField(max_length=255)
-  roast_type = models.CharField(max_length=30)
-  roaster_id = models.ForeignKey(Roasters, on_delete=models.CASCADE)
+    roast_name = models.CharField(max_length=255)
+    roast_type = models.CharField(max_length=30)
+    roaster_id = models.ForeignKey(Roasters, on_delete=models.CASCADE)
+
 
 class Orders(models.Model):
-  order_num = models.IntegerField
-  order_date = models.DateField(auto_now=False)
-  ship_date = models.DateField(auto_now=False)
-  user_id = models.ForeignKey(Users_Data, on_delete=models.CASCADE)
-
-
+    order_num = models.IntegerField
+    order_date = models.DateField(auto_now=False)
+    ship_date = models.DateField(auto_now=False)
+    user_id = models.ForeignKey(Users_Data, on_delete=models.CASCADE)
