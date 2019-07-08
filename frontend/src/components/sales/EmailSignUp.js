c6c4e1878fc5d0f28a5c2598b37c41bc3e8db7f4
Laura <laura.robertson64@gmail.com>
2019-07-07 22:53:17 -0500

    handle change on emailsignup assigns values to state

--- frontend/src/components/sales/EmailSignUp.js
+++ frontend/src/components/sales/EmailSignUp.js
@@ -11,14 +11,14 @@ const SignUpBtn = withStyles({
     background: '#e6b5da',
     '&:hover': {
       background: 'linear-gradient(30deg, #e6b5da 40%, #f5f733 60%)'
-      },
+    },
     border: 0,
     borderRadius: 3,
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     color: 'black',
     height: 48,
-    padding: '0 30px',
-  },
+    padding: '0 30px'
+  }
 })(Button);
 
 const TextFieldStyled = withStyles({
@@ -26,49 +26,53 @@ const TextFieldStyled = withStyles({
     border: '#e6b5da',
     background: '#FFFFFF',
     borderRadius: '5px',
-    width: '30vh',
-  },
+    width: '30vh'
+  }
 })(TextField);
-
 
 const EmailSignUp = () => {
   const [values, setValues] = React.useState({
     name: '',
-    age: '',
-    multiline: 'Controlled',
+    email: ''
   });
 
-  const handleChange = name => event => {
-    setValues({ ...values, [name]: event.target.value });
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
   };
 
   return (
     <div className="top-email-form">
       <form noValidate autoComplete="off">
         <div>
-        <TextFieldStyled
-          required
-          label="Name"
-          margin="normal"
-          variant="outlined"
-        />
+          <TextFieldStyled
+            required
+            name="name"
+            label="Name"
+            margin="normal"
+            variant="outlined"
+            value={values.name}
+            onChange={handleChange}
+          />
         </div>
         <div>
-        <TextFieldStyled
-          required
-          id="outlined-email-input"
-          label="Email"
-          type="email"
-          name="email"
-          autoComplete="email"
-          margin="normal"
-          variant="outlined"
-        />
+          <TextFieldStyled
+            required
+            id="outlined-email-input"
+            label="Email"
+            type="email"
+            name="email"
+            autoComplete="email"
+            margin="normal"
+            variant="outlined"
+            value={values.email}
+            onChange={handleChange}
+          />
         </div>
         <SignUpBtn> Sign Up </SignUpBtn>
       </form>
     </div>
   );
-}
+};
 
-export default EmailSignUp
+export default EmailSignUp;
