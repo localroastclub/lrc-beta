e99668d3010ad55a0912e1bb6ec8e12c54416d12
Laura <laura.robertson64@gmail.com>
2019-11-09 15:24:32 -0600

    incorporate a login context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,36 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import BaseRouter from './routes';
 import Layout from './containers/Layout';
-
-export const AuthContext = React.createContext();
-const initialState = {
-  isAuthenticated: false,
-  user: null,
-  token: null
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'LOGIN':
-      localStorage.setItem('user', JSON.stringify(action.payload.user));
-      localStorage.setItem('token', JSON.stringify(action.payload.token));
-      return {
-        ...state,
-        isAuthenticated: true,
-        user: action.payload.user,
-        token: action.payload.token
-      };
-    case 'LOGOUT':
-      localStorage.clear();
-      return {
-        ...state,
-        isAuthenticated: false,
-        user: null
-      };
-    default:
-      return state;
-  }
-};
+import { AuthContext, reducer, initialState } from './contexts/AuthContext';
 
 function App(props) {
   const [state, dispatch] = React.useReducer(reducer, initialState);
