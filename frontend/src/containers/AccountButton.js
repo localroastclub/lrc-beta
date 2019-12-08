7936ee12ba21d9d89b88e2dc1c7561d80489b988
Laura <laura.robertson64@gmail.com>
2019-12-08 13:28:20 -0600

    Add conditional rendering and functional routes to account button

--- frontend/src/containers/AccountButton.js
+++ frontend/src/containers/AccountButton.js
@@ -1,19 +1,52 @@
 import React from 'react';
+import Axios from 'axios';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthContext';
 import { withStyles } from '@material-ui/styles';
-import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
 
 const AccountIcon = withStyles({
   root: {
-    width: '5vw',
-    height: '5vh'
+    width: '4.5vw',
+    height: '4.5vh'
   }
 })(AccountBoxIcon);
 
-const AccountButton = () => {
+const AccountButton = props => {
+  const isAuthenticated = localStorage.getItem('isMember') === 'true';
+  const { dispatch } = React.useContext(AuthContext);
   const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleLogout = event => {
+    event.preventDefault();
+    console.log('we are logging out....');
+    Axios.post('http://localhost:8000/rest-auth/logout/')
+      .then(res => {
+        console.log('here is the response', res.status);
+        if (res.status === 200) {
+          return dispatch({
+            type: 'LOGOUT'
+          });
+        }
+      })
+      .then(() => {
+        console.log('in the sencond then', props);
+        window.location.href = '/';
+      })
+      .catch(err => {
+        console.log('error!', err);
+        // const errorMsg = [];
+        // _.forEach(err.response.data, e => {
+        //   errorMsg.push(e[0]);
+        // });
+        // setData({
+        //   ...data,
+        //   errorMessage: errorMsg
+        // });
+      });
+  };
 
   const handleClick = event => {
     setAnchorEl(event.currentTarget);
@@ -25,13 +58,6 @@ const AccountButton = () => {
 
   return (
     <div>
-      {/* <Button
-        aria-controls="account-menu"
-        aria-haspopup="true"
-        onClick={handleClick}
-      >
-        Open Menu
-      </Button> */}
       <AccountIcon
         aria-controls="account-menu"
         aria-haspopup="true"
@@ -44,9 +70,25 @@ const AccountButton = () => {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
+        {isAuthenticated ? (
+          <div>
+            <MenuItem onClick={handleClose}>Account</MenuItem>
+            <Link to="/dashboard">
+              <MenuItem onClick={handleClose}>My order</MenuItem>
+            </Link>
+            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+          </div>
+        ) : (
+          <div>
+            <MenuItem onClick={handleLogout}>Cart</MenuItem>
+            <Link to="/login">
+              <MenuItem onClick={handleClose}>Login</MenuItem>
+            </Link>
+            <Link to="/signup">
+              <MenuItem onClick={handleClose}>Sign up</MenuItem>
+            </Link>
+          </div>
+        )}
       </Menu>
     </div>
   );
