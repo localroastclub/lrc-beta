2588a83361f5d6c293076be88982739451e83e5b
Laura <laura.robertson64@gmail.com>
2019-12-27 21:47:04 -0600

    choose your own adventure skeleton page

--- frontend/src/components/product/ChooseYourAdventure.js
+++ frontend/src/components/product/ChooseYourAdventure.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import RoastList from './RoastList';
+import './subscription.css';
 
 // here set up the default of one option, 12 oz, no 4 oz
 // have a + icon at the bottom to add another line
@@ -7,7 +9,13 @@ import React from 'react';
 // it can then be updated or removed. If the user navigates away from the page, we want it to stay in local storage
 
 const ChooseYourAdventure = () => {
-  return <div></div>;
+  return (
+    <div className="subscription-container">
+      <div className="inner-sub-container">
+        <RoastList roastItems={roastItems} tastersTrio={true} />
+      </div>
+    </div>
+  );
 };
 
 export default ChooseYourAdventure;
