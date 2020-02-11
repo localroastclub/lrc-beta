3a4cbd494461e4ec8b2daa46ba99d3ad477ac655
Laura <laura.robertson64@gmail.com>
2020-02-10 23:23:09 -0600

    map roaster content to panels

---
+++ frontend/src/components/product/shop/Panels.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%'
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary
+  }
+}));
+
+const Panels = props => {
+  const i = props.i + 1;
+  const handleChange = props.handleChange;
+  const expanded = props.expanded;
+
+  console.log('props?', props);
+
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <ExpansionPanel
+        expanded={expanded === `panel${i}`}
+        onChange={handleChange(`panel${i}`)}
+      >
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>{props.roaster}</Typography>
+          <Typography className={classes.secondaryHeading}>
+            I am an expansion panel
+            {/* put some logo pictures in here */}
+          </Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
+            Aliquam eget maximus est, id dignissim quam.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </div>
+  );
+};
+
+export default Panels;
