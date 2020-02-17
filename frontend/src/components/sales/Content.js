38f6d1a04a6dfafa8404108b3cf54689a29fa237
Laura <laura.robertson64@gmail.com>
2020-02-17 11:29:06 -0600

    link sales page buttons to sign up if not authenticated

--- frontend/src/components/sales/Content.js
+++ frontend/src/components/sales/Content.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './content.css';
+
 import Header from './Header';
 import SubHeader from './SubHeader';
 import CopyP1 from './CopyP1';
@@ -12,27 +14,53 @@ import Reviews from './Reviews';
 import LrcButton from '../LrcButton';
 
 const Content = () => {
+  let isAuthenticated = localStorage.getItem('isMember') === 'true';
+
   return (
     <>
       <Header />
-      <div className="content">
+      <div className='content'>
         {/* <div className="content-pane"> */}
         <SubHeader />
         {/* <CopyP1 /> */}
         <CopyL1 />
-        <div id="cta-first" className="button">
-          <LrcButton className="order-btn" ctaText={'Join the Club'} />
+        <div id='cta-first' className='button'>
+          {isAuthenticated ? (
+            <Link to='/dashboard'>
+              <LrcButton className='order-btn' ctaText={'Join the Club'} />
+            </Link>
+          ) : (
+            <Link to='/signup'>
+              <LrcButton className='order-btn' ctaText={'Join the Club'} />
+            </Link>
+          )}
         </div>
         <HowItWorks />
         <CopyP2 />
-        <div id="cta-second" className="button">
-          <LrcButton ctaText={'Fresh Coffee'} />
+        <div id='cta-second' className='button'>
+          {isAuthenticated ? (
+            <Link to='/dashboard'>
+              <LrcButton ctaText={'Fresh Coffee'} />
+            </Link>
+          ) : (
+            <Link to='/signup'>
+              <LrcButton ctaText={'Fresh Coffee'} />
+            </Link>
+          )}
         </div>
         <CopyL2 />
         <ReviewLg />
         <Reviews />
-        <div className="button bottom-btn">
-          <LrcButton className="order-btn" ctaText={'Join the Club'} />
+        <div className='button bottom-btn'>
+          {isAuthenticated ? (
+            <Link to='/dashboard'>
+              <LrcButton className='order-btn' ctaText={'Join the Club'} />
+            </Link>
+          ) : (
+            <Link to='/signup'>
+              <LrcButton className='order-btn' ctaText={'Join the Club'} />
+            </Link>
+          )}
         </div>
         {/* </div> */}
       </div>
