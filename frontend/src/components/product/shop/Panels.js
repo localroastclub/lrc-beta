b5a0d4b173d2b8328d5621e82c93b3fd5f78b40c
Laura <laura.robertson64@gmail.com>
2020-02-17 16:49:49 -0600

    comment out unused import

--- frontend/src/components/product/shop/Panels.js
+++ frontend/src/components/product/shop/Panels.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import Typography from '@material-ui/core/Typography';
+// import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import RoastsGridList from './RoastsGridList';
 
