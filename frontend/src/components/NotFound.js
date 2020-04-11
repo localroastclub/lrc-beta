2dd2b6dcc7a407536ef875f5126575d5ed759c64
Laura <laura.robertson64@gmail.com>
2020-04-11 18:55:25 -0500

    set up 404 page

---
+++ frontend/src/components/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import _ from 'lodash';
+
+import './error.css';
+
+const NotFound = () => {
+  return (
+    <div className='main-container'>
+      <div className='inner-container'>
+        <h1>Sorry, page not found</h1>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
