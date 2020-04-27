0ffe9f9789dcba3511fae66416ee844aa832bdb7
Laura <laura.robertson64@gmail.com>
2020-04-26 22:15:18 -0500

    fetch roster data from api, dynamically render logo

--- frontend/src/components/product/RoastListItem.js
+++ frontend/src/components/product/RoastListItem.js
@@ -1,4 +1,6 @@
-import React, { useEffect } from 'react';
+import axios from 'axios';
+import _ from 'lodash';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 // import CardActionArea from '@material-ui/core/CardActionArea';
@@ -11,7 +13,7 @@ import Typography from '@material-ui/core/Typography';
 import Menu from './Menu';
 import '../userdashboard/itemcard.css';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   card: {
     background: '#fffcfe',
     display: 'flex',
@@ -26,24 +28,24 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.down(840)]: {
       width: '70vw',
       flexDirection: 'column',
-      alignItems: 'center'
+      alignItems: 'center',
     },
     [theme.breakpoints.down(600)]: {
       // height: '115vh',
       width: '70vw',
       flexDirection: 'column',
-      alignItems: 'center'
-    }
+      alignItems: 'center',
+    },
   },
   headerSpace: {
     display: 'flex',
     width: '100%',
     background: '#6A7933',
-    boxShadow: '2px 2px 4px rgba(0, 0, 0, .5)'
+    boxShadow: '2px 2px 4px rgba(0, 0, 0, .5)',
   },
   header: {
     display: 'flex',
-    width: '100%'
+    width: '100%',
   },
   content: {
     display: 'flex',
@@ -53,101 +55,118 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.down(840)]: {
       width: '60vw',
       flexDirection: 'column',
-      alignItems: 'center'
+      alignItems: 'center',
     },
     [theme.breakpoints.down(600)]: {
-      minHeight: '105vh',
+      minHeight: '90vh',
       width: '70vw',
       flexDirection: 'column',
-      alignItems: 'center'
-    }
+      alignItems: 'center',
+    },
+  },
+  details: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'left',
+    marginLeft: '2%',
+    width: '100%',
+    [theme.breakpoints.down(840)]: {
+      width: '60vw',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    [theme.breakpoints.down(600)]: {
+      minHeight: '50vh',
+      width: '70vw',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
   },
   paragraph: {
     display: 'flex',
     justifyItems: 'left',
     [theme.breakpoints.down(940)]: {
-      margin: 3
-    }
+      margin: 3,
+    },
   },
   image: {
     margin: 10,
     marginRight: 20,
     width: 151,
-    height: 151
+    height: 151,
+    objectFit: 'scale-down',
   },
   root: {
     fontFamily: 'Lato',
     marginBottom: '.2em',
-    fontSize: '4vh'
+    fontSize: '4vh',
   },
   text: {
     fontFamily: 'Lato',
     fontSize: '2.5vh',
     [theme.breakpoints.down(940)]: {
-      fontSize: '2.5vh'
-    }
+      fontSize: '2.5vh',
+    },
   },
   actions: {
     display: 'flex',
     justifyContent: 'flex-end',
     alignItems: 'flex-end',
     flexFlow: 'wrap',
+    marginRight: '2%',
     width: '35vw',
     [theme.breakpoints.down(840)]: {
       flexDirection: 'column',
       alignItems: 'center',
       justifyContent: 'center',
-      width: '35vw'
+      width: '35vw',
     },
     [theme.breakpoints.down(600)]: {
       flexDirection: 'column',
       alignItems: 'center',
       justifyContent: 'center',
-      width: '50vw'
-    }
+      width: '50vw',
+    },
   },
   button: {
-    fontFamily: 'Lato'
-  }
+    fontFamily: 'Lato',
+  },
 }));
 
-const roasterList = [
-  "Barrett's",
-  'Little City',
-  'Casa Brasil',
-  'Greater Goods',
-  'Trianon',
-  "Mozart's",
-  'Club Choice'
-];
 const roastList = [
   'Light',
   'Medium',
   'Dark',
   'Espresso',
   'Decaf',
-  'Club Choice'
+  'Club Choice',
 ];
 const beanStatusList = ['Whole', 'Ground'];
 const originList = ['Single', 'Blend'];
 const bagSizeList = ['12 oz', '4 oz'];
 
-const RoastListItem = props => {
+const RoastListItem = (props) => {
   const classes = useStyles();
   const alt = `${props.item.roaster} ${props.item.roastType} roast ${props.item.beanStatus} bean`;
   const customSelection = props.customSelection;
   const tastersTrio = props.tastersTrio;
+  const [roasterList, setRoasterList] = useState([]);
+  const [roasterData, setRoasterData] = useState({});
 
-  const [value, setValue] = React.useState({
+  const [value, setValue] = useState({
     roaster: props.item.roaster,
     roast: props.item.roast,
     bean: props.item.bean,
     origin: props.item.origin,
-    size: props.item.size
+    size: props.item.size,
   });
 
-  const handleChange = event => {
+  const [logo, setLogo] = useState('');
+
+  const handleChange = (event) => {
     const property = event.target.name;
+    console.log('property', event.target.name, '\n', event.target.value);
     setValue({ ...value, [property]: event.target.value });
     props.item[property] = event.target.value;
     console.log('event value', event.target.value);
@@ -172,6 +191,29 @@ const RoastListItem = props => {
       : localStorage.setItem('orderChoice', JSON.stringify(storageItems));
   };
 
+  const host = 'http://localhost:8000';
+
+  useEffect(() => {
+    axios.get(`${host}/api/roaster`).then((resp) => {
+      console.log('what si resp?', resp);
+      setRoasterList(
+        _.map(resp.data, (el) => {
+          return el.name;
+        })
+      );
+      setRoasterData(
+        _.reduce(
+          resp.data,
+          (acc, el, key) => {
+            acc[el.name] = { logo: el.logo_url };
+            return acc;
+          },
+          {}
+        )
+      );
+    });
+  }, []);
+
   useEffect(() => {
     setValue(props.item);
   }, [value, props.item]);
@@ -187,8 +229,8 @@ const RoastListItem = props => {
           {customSelection ? (
             <Button
               className={classes.button}
-              size="medium"
-              color="inherit"
+              size='medium'
+              color='inherit'
               onClick={() => {
                 props.removeItem(props.index - 1);
               }}
@@ -198,23 +240,29 @@ const RoastListItem = props => {
           ) : null}
         </div>
         <div className={classes.content}>
-          <CardMedia
-            className={classes.image}
-            component="img"
-            alt={alt}
-            height="140"
-            // image={props.item.imageUrl}
-            // title={props.item.roaster}
-          />
-          <CardContent className={classes.paragraph}>
-            <div className={classes.text}>
-              <p>Roaster: {value.roaster}</p>
-              <p>Roast Type: {value.roast}</p>
-              <p>Beans: {value.bean}</p>
-              <p>Origin: {value.origin}</p>
-              <p>Bag Size: {value.size}</p>
-            </div>
-          </CardContent>
+          <div className={classes.details}>
+            <CardMedia
+              className={classes.image}
+              component='img'
+              alt={alt}
+              height='140'
+              image={
+                roasterData[value.roaster]
+                  ? roasterData[value.roaster]['logo']
+                  : ''
+              }
+              // title={props.item.roaster}
+            />
+            <CardContent className={classes.paragraph}>
+              <div className={classes.text}>
+                <p>Roaster: {value.roaster}</p>
+                <p>Roast Type: {value.roast}</p>
+                <p>Beans: {value.bean}</p>
+                <p>Origin: {value.origin}</p>
+                <p>Bag Size: {value.size}</p>
+              </div>
+            </CardContent>
+          </div>
           <CardActions className={classes.actions}>
             <Menu
               value={value.roaster}
