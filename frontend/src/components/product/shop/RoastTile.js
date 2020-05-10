61a038e0ddac35275be270c7b6606bc5d303e606
Laura <laura.robertson64@gmail.com>
2020-05-10 17:05:43 -0500

    factor each grid item into its own component

---
+++ frontend/src/components/product/shop/RoastTile.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import AddShoppingCartOutlinedIcon from '@material-ui/icons/AddShoppingCartOutlined';
+
+import OrderModal from './OrderModal';
+
+const useStyles = makeStyles((theme) => ({
+  itemSquare: {
+    width: 200,
+    height: 200,
+  },
+  image: {
+    objectFit: 'scale-down',
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+      'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+  icon: {
+    color: 'white',
+  },
+}));
+
+const RoastTile = (props) => {
+  const classes = useStyles();
+  const { tile } = props;
+
+  const [openOrderModal, setOpenOrderModal] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpenOrderModal(true);
+  };
+
+  const handleClose = () => {
+    setOpenOrderModal(false);
+  };
+  return (
+    <div>
+      <GridListTile
+        key={props.i}
+        cols={tile.featured ? 2 : 1}
+        rows={tile.featured ? 2 : 1}
+        className={classes.itemSquare}
+      >
+        <img src={tile.img} alt={tile.title} className={classes.image} />
+        <GridListTileBar
+          title={tile.title}
+          titlePosition='bottom'
+          subtitle={<span>{tile.description}</span>}
+          actionIcon={
+            <IconButton
+              aria-label={`star ${tile.title}`}
+              className={classes.icon}
+            >
+              <AddShoppingCartOutlinedIcon />
+            </IconButton>
+          }
+          actionPosition='left'
+          className={classes.titleBar}
+          onClick={handleOpen}
+        />
+        {openOrderModal ? (
+          <OrderModal
+            handleClose={handleClose}
+            open={openOrderModal}
+            p={tile.title}
+          />
+        ) : null}
+      </GridListTile>
+    </div>
+  );
+};
+
+export default RoastTile;
--- frontend/src/components/product/shop/RoastsGridList.js
+++ frontend/src/components/product/shop/RoastsGridList.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
-import AddShoppingCartOutlinedIcon from '@material-ui/icons/AddShoppingCartOutlined';
-// import tileData from './tileData';
+
+import RoastTile from './RoastTile';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -36,21 +32,6 @@ const useStyles = makeStyles((theme) => ({
       flexDirection: 'column',
       alignItems: 'center',
     },
-  },
-  itemSquare: {
-    minWidth: 200,
-    minHeight: 200,
-  },
-  image: {
-    objectFit: 'scale-down',
-  },
-  titleBar: {
-    background:
-      'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
-      'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-  },
-  icon: {
-    color: 'white',
   },
 }));
 
@@ -84,30 +65,8 @@ const RoastsGridList = (props) => {
         spacing={1}
         className={classes.gridList}
       >
-        {tileData.map((tile) => (
-          <GridListTile
-            key={tile.img}
-            cols={tile.featured ? 2 : 1}
-            rows={tile.featured ? 2 : 1}
-            className={classes.itemSquare}
-          >
-            <img src={tile.img} alt={tile.title} className={classes.image} />
-            <GridListTileBar
-              title={tile.title}
-              titlePosition='bottom'
-              subtitle={<span>{tile.description}</span>}
-              actionIcon={
-                <IconButton
-                  aria-label={`star ${tile.title}`}
-                  className={classes.icon}
-                >
-                  <AddShoppingCartOutlinedIcon />
-                </IconButton>
-              }
-              actionPosition='left'
-              className={classes.titleBar}
-            />
-          </GridListTile>
+        {tileData.map((tile, i) => (
+          <RoastTile tile={tile} key={i} i={i} />
         ))}
       </GridList>
     </div>
